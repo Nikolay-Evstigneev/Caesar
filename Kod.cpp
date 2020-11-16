abe1e851517b45313b23cb297ca2a7f989ce0bf7
Nikolay-Evstigneev <71175033+Nikolay-Evstigneev@users.noreply.github.com>
2020-11-16 14:05:03 +0500

    Добавлена запись в Логгер

--- Kod.cpp
+++ Kod.cpp
@@ -1,19 +1,31 @@
-# include  < строка >
-# включить  < iostream >
+# include  < string >
+# include  < iostream >
 # include  < Windows.h >
-используя  пространство имен  std ; //блок библиотек
-int  main ()
+# include  <ctime>
+# include  <fstream>
+using namespace std; //блок библиотек
+string time() 
 {
-	setlocale (LC_ALL, « русский » );
-	строка строка, ответ, команда, rule;
-	символ char ;
+	char str[26];
+	time_t result = time(NULL);
+	ctime_s(str, sizeof str, &result);
+	return str;
+}
+int  main()
+{
+	string path = "log.txt";
+	setlocale(LC_ALL, "Russian");
+	string line, answer, command, rule;
+	char symbol ;
 	int sh;
 	bool T; //описание переменных
-	do 
+	ofstream fout(path, ios_base::app);
+	fout << "Program is started : " << time();
+	do
 	{
-	        T = true;
+		T = true;
 		cout << "Введите шаг сдвига: ";
-		cin >> rule; //ввод данных
+		getline(cin, rule); //ввод данных
 		for (int i = 0; i < rule.length(); i++) {
 			if ((!isdigit(rule[i])) and (rule[i] != '-')) {
 				T = false;
@@ -23,33 +35,47 @@ int  main ()
 		}
 	} while (!T); //проверка ввода
 	sh = atoi(rule.c_str());
-	делать
+	do
 	{
-		cout << " Введите encode для кодирования или decode для расшифровки: " << endl;
-		cin >> команда;
-		если (команда == " декодировать " ) sh * = - 1 ;
-	} //блок ввода команды
-	while ((команда! = " кодировать " ) и (команда! = " декодировать " ));
-	cout << " Введите строку: " << endl;
-	cin >> line; //ввод строки
-	ответ. изменить размер (строка. длина ());
-	для ( int i = 0 ; i <line. length (); i ++) //блок работы со строкой
-	{
-		если ((строка [i]> 96 ) и ((строка [i] < 123 ))
+		cout << "Введите encode для кодирования или decode для расшифровки: " << endl;
+		getline(cin, command); //получение команды
+		if (command == "decode") { sh *= -1; } //блок ввода команды
+		else if (command != "encode") 
 		{
-			символ = строка [i] + = sh;
-			символ = символ ( 97 + ( 26 * ( 1 - (символ / 97 ))) + ((символ - 97 )% 26 ));
-			ответ [i] = символ;
-		}
-		else  if ((строка [i]> 64 ) и (строка [i] < 91 )) {
-			символ = строка [i] + = sh;
-			символ = символ ( 64 + ( 26 * ( 1 - (символ / 64 ))) + ((символ - 64 )% 26 ));
-			ответ [i] = символ;
-		}
-		еще 
-		{
-			ответ [i] = строка [i]; //присваивание
+			cout << "Неверный ввод. Попробуйте снова." << endl;
+			fout << "Invalid enter.: " << time();
 		}
 	}
-	cout << " Oтвет: " << ответ; //вывод измененной строки
+	while ((command!= "encode") and(command!= "decode"));
+	cout << "Введите строку: " << endl;
+	getline(cin, line); //ввод строки
+	answer.resize(line.length());
+	for(int i = 0; i < line.length(); i++) //блок работы со строкой
+	{
+		if((line[i] > 96) and((line[i] < 123)))
+		{
+			symbol = line[i] += sh;
+			symbol = char(97 + (26 * (1 - (symbol / 97))) + ((symbol - 97) % 26));
+			answer[i] = symbol;
+		}
+		else  if ((line[i] > 64) and(line[i] < 91)) {
+		symbol = line[i] += sh;
+		symbol = char(64 + (26 * (1 - (symbol / 64))) + ((symbol - 64) % 26));
+		answer[i] = symbol;
+		}
+		else
+		{
+			answer[i] = line[i]; //присваивание
+		}
+	}
+	cout << "Oтвет: " << answer; //вывод измененной строки
+	if (command == "encode") {
+		fout << "Line is encoded : " << time();
+	}
+	else {
+		fout << "Line is decoded : " << time();
+	}
+	fout << "Program finished : " << time();
+	fout << endl;
+	fout.close();
 }
